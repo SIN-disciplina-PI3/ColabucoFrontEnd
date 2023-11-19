92cdaac9172a30ce2de328c9dc917c26ce83a9b7
LorenzoLeao <lrzo.leao@gmail.com>
2023-11-19 20:38:11 -0300

    cadastre-se incompleto

--- src/pages/CadastroCliente.jsx
+++ src/pages/CadastroCliente.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from './/..//components/Navbar'
-import { Stack, Text, Button, Input,FormControl, FormLabel } from "@chakra-ui/react";
+import { Stack, Text, Button, Input, FormControl, FormLabel } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import styles from './../styles/cadastrocliente.css'
 
@@ -9,15 +9,34 @@ import styles from './../styles/cadastrocliente.css'
 function CadastroCliente() {
 
     return (
-        
+
         <Stack>
             <NavBar />
 
-            <Text pb='5%' className="texto05"> CADASTRE-SE </Text>
+            <Text mt='-8px' borderWidth='2.4px'  borderTopColor='black' borderBottomColor='black' borderLeftColor='#6600CC'
+            borderRightColor='#6600CC'  
+            className="texto05" height='150' > CADASTRE-SE </Text>
 
-            <FormControl isRequired>
-                <FormLabel>First name</FormLabel>
-                <Input placeholder='First name' />
+            <FormControl isRequired pl='30%' >
+
+
+                <FormLabel className='formulario'  >Nome</FormLabel>
+                <Input width='50%' placeholder='Nome' />
+
+
+
+                <FormLabel className='formulario'  >Nome</FormLabel>
+                <Input width='50%' placeholder='Nome' />
+
+
+
+                <FormLabel className='formulario'  >Nome</FormLabel>
+                <Input width='50%' placeholder='Nome' />
+
+
+
+                <FormLabel className='formulario'  >Nome</FormLabel>
+                <Input width='50%' placeholder='Nome' />
             </FormControl>
 
         </Stack>
