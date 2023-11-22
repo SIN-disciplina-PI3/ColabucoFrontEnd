e6fa7bc125f1c011b77e97f363d9d9e846fb886c
Nicholas-Bergqvist <nicholas.2021150430@unicap.br>
2023-11-21 23:08:14 -0300

    retirada do componente do App.jsx para teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes.jsx";
-import ArtistRegister from "./components/ArtistRegister.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <AppRoutes /> */}
-      <ArtistRegister />
+      <AppRoutes />
     </BrowserRouter>
   );
 }
