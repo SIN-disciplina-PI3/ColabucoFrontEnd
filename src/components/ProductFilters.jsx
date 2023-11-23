723c1152f75bde2c134447cc0035363b5ed2df8c
louixpng <luismsm2003@gmail.com>
2023-11-23 09:45:23 -0300

    Criado o componente SectionTitleFilters, adicionado no ProductFilters

--- src/components/ProductFilters.jsx
+++ src/components/ProductFilters.jsx
@@ -9,44 +9,53 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
+  Center,
 } from '@chakra-ui/react'
 
 import sandwich from '../assets/sandwich.svg';
 
-import {Button, Input, useDisclosure} from '@chakra-ui/react'
+import sandwich2 from '../assets/sandwich2.svg';
+
+import SectionTitle from './SectionTitle';
+
+import { Button, Input, useDisclosure } from '@chakra-ui/react'
 
 import '../styles/home.css';
+import SectionTitleFilters from './SectionTitleFilters';
 
 function ProductFilters() {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const btnRef = React.useRef()
- 
+
   return (
     <>
-    
+
       <Button ref={btnRef} colorScheme='#FFFEEF' onClick={onOpen}>
-        <Image src = {sandwich}></Image>
+        <Image src={sandwich}></Image>
       </Button>
       <Drawer
         isOpen={isOpen}
         placement='left'
         onClose={onClose}
         finalFocusRef={btnRef}
-        size='md'  
+        size='md'
       >
         <DrawerOverlay />
         <DrawerContent>
-          <DrawerCloseButton>
-            <Image src = {sandwich}></Image>
-          </DrawerCloseButton>
 
-          <DrawerHeader className='montserrat700' textAlign="center" fontSize="32px" border='solid 5px black'>Filtros</DrawerHeader>
+          <DrawerHeader h='136px'className='montserrat700' fontSize="32px" border='solid 5px black' textAlign="center">Filtros
+            <DrawerCloseButton w='45' h='27' marginLeft="auto" marginRight="auto" display='block'>
+              <Image src={sandwich2}></Image>
+            </DrawerCloseButton>
+          </DrawerHeader>
 
           <DrawerBody align='center' borderX='solid 5px black' pt='32px'>
             <Button
-            w='353px'h='65px' border='solid 3px black' borderRadius='8px' bg='#0099FF' 
-            className='montserrat600' fontSize="20px" colorScheme='buttonscheme' textColor='black'
-          > PROMOÇÃO </Button>
+              w='353px' h='65px' border='solid 3px black' borderRadius='8px' bg='#0099FF' borderBottom="solid 5px" borderLeft="solid 5px"
+              className='montserrat600' fontSize="20px" colorScheme='buttonscheme' textColor='black'
+            > PROMOÇÃO </Button>
+
+            <SectionTitleFilters title='Categorias' />
           </DrawerBody>
 
           {/* <DrawerFooter>
@@ -56,7 +65,7 @@ function ProductFilters() {
             <Button colorScheme='blue'>Save</Button>
           </DrawerFooter> */}
         </DrawerContent>
-        
+
       </Drawer>
     </>
   )
---
+++ src/components/SectionTitleFilters.jsx
@@ -0,0 +1,13 @@
+import { Box, Text } from '@chakra-ui/react'
+
+function SectionTitleFilters(props){
+    return(
+        <Box mt='64px' w={'100%'} h={'100%'} position='relative'>
+            <Text position='absolute' w='188px' h='61px' py='12px' className='montserrat700' fontSize='20px' bg='#DFF362' border='solid 3px black' borderRadius='100'>
+                {props.title}
+            </Text>
+        </Box>
+    );
+}
+
+export default SectionTitleFilters;
