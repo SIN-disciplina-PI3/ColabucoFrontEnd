fded7bd234d3b9a943b948b2ba6ee2d54b2512b4
Nicholas-Bergqvist <nicholas.2021150430@unicap.br>
2023-11-27 16:28:57 -0300

    funcionalidade de input cpf/cnpj e estilos extras

--- src/components/ArtistRegister.jsx
+++ src/components/ArtistRegister.jsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Text,
-  Heading,
   Stack,
   Input,
   Flex,
@@ -21,6 +20,8 @@ function ArtistRegister() {
   const [password, setPassword] = useState("");
   const [passwordValid, setPasswordValid] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState(true);
+  const [cpfCnpj, setCpfCnpj] = useState("");
+  const [confirmCpfCnpj, setConfirmCpfCnpj] = useState(true);
 
   function handleClickPassword() {
     setShow(!show);
@@ -42,11 +43,36 @@ function ArtistRegister() {
   }
 
   function handlePasswordCheck(e) {
-    if (e.target.value == password) {
+    if (e.target.value === password) {
       setConfirmPassword(false);
       alert("Senha confere");
     } else {
       setConfirmPassword(true);
+    }
+  }
+
+  function handleCpfCnpj(e) {
+    const cpfCnpjCheck = e.target.value;
+    console.log(cpfCnpjCheck);
+    console.log(cpfCnpjCheck.length);
+    const checkCnpj = /([0-9]{2}?[0-9]{3}?[0-9]{3}?[0-9]{4}?[0-9]{2})/g;
+
+    const checkCpf = /([0-9]{3}?[0-9]{3}?[0-9]{3}?[0-9]{2})/g;
+
+    if (checkCpf.test(cpfCnpjCheck) && cpfCnpjCheck.length === 11) {
+      setCpfCnpj(cpfCnpj);
+      setConfirmCpfCnpj(false);
+
+      alert("Válido");
+    } else {
+      setConfirmCpfCnpj(true);
+    }
+    if (checkCnpj.test(e.target.value) && cpfCnpjCheck.length === 14) {
+      setCpfCnpj(cpfCnpj);
+      setConfirmCpfCnpj(false);
+      alert("Válido");
+    } else {
+      setConfirmCpfCnpj(true);
     }
   }
 
@@ -69,29 +95,64 @@ function ArtistRegister() {
       >
         <FormLabel>Nome da empresa ou vendedor:</FormLabel>
         <Input
-          focusBorderColor={"#6600CC"}
           border="2px"
           placeholder="Digite nome da empresa ou vendedor"
+          _hover={{ borderColor: "#6600CC" }}
+          focusBorderColor={"#6600CC"}
         />
 
-        <FormLabel>CPF ou CNPJ:</FormLabel>
-        <Input border="2px" placeholder="Digite cpf ou cnpj"></Input>
+        <FormLabel paddingTop={4}>CPF ou CNPJ:</FormLabel>
+        <Input
+          border="2px"
+          placeholder="Digite cpf ou cnpj"
+          onChange={handleCpfCnpj}
+          _hover={{ borderColor: "#6600CC" }}
+          focusBorderColor={"#6600CC"}
+          isInvalid={confirmCpfCnpj}
+        />
+        <Text>{cpfCnpj}</Text>
 
-        <FormLabel>Telefone:</FormLabel>
-        <Input border="2px" placeholder="ddd + número" />
+        <FormLabel paddingTop={4}>Telefone:</FormLabel>
+        <Input
+          border="2px"
+          placeholder="ddd + número"
+          _hover={{ borderColor: "#6600CC" }}
+          focusBorderColor={"#6600CC"}
+        />
 
-        <FormLabel>Email:</FormLabel>
-        <Input border="2px" type="email" placeholder="Digite email" />
+        <FormLabel paddingTop={4}>Email:</FormLabel>
+        <Input
+          border="2px"
+          type="email"
+          placeholder="Digite email"
+          _hover={{ borderColor: "#6600CC" }}
+          focusBorderColor={"#6600CC"}
+        />
 
-        <FormLabel>Endereço:</FormLabel>
-        <Input border="2px" placeholder="Endereço com número" />
+        <FormLabel paddingTop={4}>Endereço:</FormLabel>
+        <Input
+          border="2px"
+          placeholder="Endereço com número"
+          _hover={{ borderColor: "#6600CC" }}
+          focusBorderColor={"#6600CC"}
+        />
 
         <Box paddingTop={4}>
           <Flex gap={2}>
-            <Select border="2px" placeholder="Selecione o Estado">
+            <Select
+              border="2px"
+              placeholder="Selecione o Estado"
+              _hover={{ borderColor: "#6600CC" }}
+              focusBorderColor={"#6600CC"}
+            >
               <option value="option1">Pernambuco</option>
             </Select>
-            <Select border="2px" placeholder="Selecione a Cidade">
+            <Select
+              border="2px"
+              placeholder="Selecione a Cidade"
+              _hover={{ borderColor: "#6600CC" }}
+              focusBorderColor={"#6600CC"}
+            >
               <option value="option1">Recife</option>
               <option value="option2">Olinda</option>
               <option value="option3">Paulista</option>
@@ -99,38 +160,51 @@ function ArtistRegister() {
           </Flex>
         </Box>
 
-        <FormLabel>Selecione o(s) tipo(s) de arte que você produz:</FormLabel>
-        <Select border="2px" placeholder="Selecione o tipo">
+        <FormLabel paddingTop={4}>
+          Selecione o(s) tipo(s) de arte que você produz:
+        </FormLabel>
+        <Select
+          border="2px"
+          placeholder="Selecione o tipo"
+          _hover={{ borderColor: "#6600CC" }}
+          focusBorderColor={"#6600CC"}
+        >
           <option value="option1">Música</option>
           <option value="option2">Literatura</option>
           <option value="option3">Cinema</option>
           <option value="option4">Outros</option>
         </Select>
 
-        <FormLabel>
+        <FormLabel paddingTop={4}>
           Políticas de entrega e frete (incluindo prazos de entrega)
         </FormLabel>
         <Select
           border="2px"
           placeholder="Selecione tipo de política de entrega e frete"
+          _hover={{ borderColor: "#6600CC" }}
+          focusBorderColor={"#6600CC"}
         ></Select>
 
-        <FormLabel>
+        <FormLabel paddingTop={4}>
           Uma descrição da sua loja, incluindo o tipo de produtos ou serviços
           que você oferece.
         </FormLabel>
         <Textarea
           border="2px"
           placeholder="Descrição de loja, produtos, serviços ..."
+          _hover={{ borderColor: "#6600CC" }}
+          focusBorderColor={"#6600CC"}
         />
 
-        <FormLabel>Senha:</FormLabel>
+        <FormLabel paddingTop={4}>Senha:</FormLabel>
         <InputGroup>
           <Input
             border="2px"
             type={show ? "text" : "password"}
             onChange={handlePassword}
             placeholder="Digite a senha"
+            _hover={{ borderColor: "#6600CC" }}
+            focusBorderColor={"#6600CC"}
           />
           <InputRightElement width="8rem">
             <Button
@@ -147,7 +221,7 @@ function ArtistRegister() {
 
         {passwordValid ? <Text>✔</Text> : <Text>Mínimo de 8 caracteres</Text>}
 
-        <FormLabel>Confirmar Senha:</FormLabel>
+        <FormLabel paddingTop={4}>Confirmar Senha:</FormLabel>
         <InputGroup>
           <Input
             border="2px"
@@ -155,6 +229,8 @@ function ArtistRegister() {
             placeholder="Confirme a senha"
             isInvalid={confirmPassword}
             onChange={handlePasswordCheck}
+            _hover={{ borderColor: "#6600CC" }}
+            focusBorderColor={"#6600CC"}
           />
           <InputRightElement width="8rem">
             <Button
@@ -171,7 +247,7 @@ function ArtistRegister() {
 
         {confirmPassword ? <Text>Senha não esta igual.</Text> : <Text>✔</Text>}
 
-        <Box>
+        <Box paddingTop={4}>
           <Text color={"#9E9E9E"}>
             Por favor, leia atentamente os termos e condições abaixo antes de
             prosseguir com o cadastro da sua loja no nosso marketplace. Ao
@@ -183,7 +259,7 @@ function ArtistRegister() {
           </Text>
         </Box>
 
-        <Checkbox defaultUnChecked paddingBottom={4}>
+        <Checkbox defaultChecked paddingBottom={4}>
           Li e concordo com os Termos e Condições da Plataforma
         </Checkbox>
 
