208c6d9198e6baa594c98128eb6080d22df68175
Nicholas-Bergqvist <nicholas.2021150430@unicap.br>
2023-12-10 22:19:57 -0300

    Adicionado componente de artista na pagina todos artistas

--- src/pages/AllArtists.jsx
+++ src/pages/AllArtists.jsx
@@ -1,18 +1,41 @@
-import { Center, Image } from "@chakra-ui/react"
-import allartists from '../assets/allartists.svg';
+import { Box, Button, Center, Image, Flex, Text } from "@chakra-ui/react";
+import allartists from "../assets/allartists.svg";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ArtistProfileCard from "../components/ArtistProfileCard";
 
-function AllArtists(){
-    return(
-        <>
-        <Navbar />
-        <Center>
-            <Image mt='48px'src={allartists}></Image>
-        </Center>
-        <Footer />
-        </>
-    )
+function AllArtists() {
+  const texto =
+    "Olá! Sou o Falafel, um jovem artista e estudante de Expressão Gráfica na UFPE. Minha paixão pela criação visual se traduz em belos posters que refletem minha jornada criativa e inspiração. No Colabuco, compartilho minha arte de forma independente, cada peça é única e representa meu amor pela expressão gráfica. Cada compra ajuda a apoiar meu sonho artístico e me permite continuar a criar e inovar. Espero que você encontre algo especial em meu trabalho para decorar seu espaço ou presentear alguém. Bem-vindo à minha loja, e obrigado por apoiar o mundo da arte independente. 🦀🏖";
+  return (
+    <>
+      <Navbar />
+      <Center>
+        <Image mt="48px" src={allartists}></Image>
+      </Center>
+      <ArtistProfileCard
+        linkRedesArtista="@linkartista"
+        descricao={texto}
+        corFundo="#FFFF00"
+      />
+      <ArtistProfileCard
+        linkRedesArtista="@linkartista"
+        descricao={texto}
+        corFundo="#FF6600"
+      />
+      <ArtistProfileCard
+        linkRedesArtista="@linkartista"
+        descricao={texto}
+        corFundo="#0099FF"
+      />
+      <ArtistProfileCard
+        linkRedesArtista="@linkartista"
+        descricao={texto}
+        corFundo="#FF24BD"
+      />
+      <Footer />
+    </>
+  );
 }
 
-export default AllArtists
+export default AllArtists;
